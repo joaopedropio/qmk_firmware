b166794f204e474e6376806c14ed7b42e7b18c48
joaopedropio <joao-pio@hotmail.com>
2021-12-14 09:50:18 -0300

    Add rgb per layer

--- keyboards/kprepublic/bm40hsrgb/keymaps/joaopedropio/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/joaopedropio/keymap.c
@@ -202,3 +202,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______,      _______,     _______, _______,  _______, _______, _______
     )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = biton32(state);
+    switch (layer) {
+        case _GAMER:
+        case _GAMER_NUMBERS:
+            rgblight_mode(22);
+            break;
+        default:
+            rgblight_mode(25);
+    }
+
+    return state;
+}
