721ae2955b56d4b0f3265876b8bf20b3374b8008
Joao Pedro Pio <joao-pio@hotmail.com>
2024-04-15 01:01:19 -0300

    Refactoring animations code

--- keyboards/bluebell/swoop/keymaps/joaopedropio/rules.mk
+++ keyboards/bluebell/swoop/keymaps/joaopedropio/rules.mk
@@ -11,3 +11,43 @@ SPLIT_KEYBOARD = yes
 OLED_ENABLE = yes
 OLED_DRIVER_ENABLE = yes
 WPM_ENABLE = yes
+
+OCEAN_DREAM_ENABLE = no
+LUNA_ENABLE = no
+BONGOCAT_ENABLE = yes
+
+ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
+
+	ifdef OCEAN_DREAM_ENABLE
+		ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+			SRC += ocean_dream.c
+			OPT_DEFS += -DOCEAN_DREAM_ENABLE
+    	endif
+	endif
+	ifndef OCEAN_DREAM_ENABLE
+		SRC += ocean_dream.c
+		OPT_DEFS += -DOCEAN_DREAM_ENABLE
+	endif
+
+	ifdef LUNA_ENABLE
+		ifeq ($(strip $(LUNA_ENABLE)), yes)
+			SRC += luna.c
+			OPT_DEFS += -DLUNA_ENABLE
+		endif
+	endif
+	ifndef LUNA_ENABLE
+		SRC += luna.c
+		OPT_DEFS += -DLUNA_ENABLE
+	endif
+
+	ifdef BONGOCAT_ENABLE
+		ifeq ($(strip $(BONGOCAT_ENABLE)), yes)
+			SRC += bongocat.c
+			OPT_DEFS += -DBONGOCAT_ENABLE
+		endif
+	endif
+	ifndef BONGOCAT_ENABLE
+		SRC += bongocat.c qRFRrstffftssfssRfptt  ttffssrrftCS/
+		OPT_DEFS += -DBONGOCAT_ENABLE
+	endif
+endif
